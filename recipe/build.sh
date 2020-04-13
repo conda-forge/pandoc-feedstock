481193983980dea25f70e846a26ae6e6ff06d965
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2020-04-13 19:35:51 +0700

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 mkdir -p ${PREFIX}/bin
 
-if [ $(uname) == Linux ]; then
+if [ "$(uname)" == "Linux" ]; then
     mv bin/* ${PREFIX}/bin
 fi
 
 
-if [ $(uname) == Darwin ]; then
+if [ "$(uname)" == "Darwin" ]; then
     pkgutil --expand pandoc.pkg pandoc
     cpio -i -I pandoc/pandoc.pkg/Payload
-    cp usr/local/bin/* ${PREFIX}/bin/
+    cp usr/local/bin/pandoc ${PREFIX}/bin/
+    cp usr/local/bin/pandoc-citeproc ${PREFIX}/bin/
 fi
