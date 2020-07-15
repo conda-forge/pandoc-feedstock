80ef27bee6c4630f384986200bac42598041ad78
step21 <step21@devtal.de>
2020-07-15 23:53:31 +0200

    fixing windows binary location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,6 @@ stack setup
 stack install
 
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
-
-copy %TEMP%\Pandoc\*.exe %LIBRARY_BIN% || exit 1
+echo %LIBRARY_BIN%
+rem AppData\Roaming\local\bin
+copy %APPDATA%\local\bin\*.exe %LIBRARY_BIN% || exit 1
