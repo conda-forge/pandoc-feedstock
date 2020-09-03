d4e8e19a4816ba16ec2daf3bb4dc3812587abe69
step21 <step21@users.noreply.github.com>
2020-09-03 22:14:32 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #set -euo pipefail
 #IFS=$'\n\t'
-echo ${PREFIX}
+echo $BUILD_PREFIX
+echo "----------------\n"
+ls $BUILD_PREFIX
+echo "----------------\n"
+ls $SRC_PREFIX
+echo "----------------\n"
+ls $BUILD_PREFIX/lib/ghc-8.2.2/include
+echo "----------------\n"
 #mkdir -p ${PREFIX}/bin
 #mv bin/* ${PREFIX}/bin
 #stack setup
