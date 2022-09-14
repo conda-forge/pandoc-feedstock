0244e3561b886463e101fd0115aa6097900b393d
Marco Rossi <developer@marco-rossi.com>
2022-09-14 12:46:19 +0200

    Repackage binary on aarch64 as stack is not yet available there
    https://github.com/conda-forge/stack-feedstock/pull/35

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
-stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install pandoc
 mkdir -p ${PREFIX}/bin
-mv ~/.local/bin/pandoc ${PREFIX}/bin
+if [[ "$target_platform" == linux-aarch64 ]]; then
+    mv bin/* ${PREFIX}/bin
+else
+    stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install pandoc
+    mv ~/.local/bin/pandoc ${PREFIX}/bin
+fi
