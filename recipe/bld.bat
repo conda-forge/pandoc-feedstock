27adc9abc6d27aef01f841fef5cb0bedcd911e83
Marco Rossi <developer@marco-rossi.com>
2022-09-22 21:35:03 +0200

    Add ghc build options

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
-stack install pandoc
+stack install --ghc-options='%GHC_OPTIONS%' pandoc
 MKDIR %PREFIX%\bin
 MOVE %APPDATA%\local\bin\pandoc.exe %PREFIX%\bin\pandoc.exe
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@ mkdir -p ${PREFIX}/bin
 if [[ "$target_platform" == linux-aarch64 ]]; then
     mv bin/* ${PREFIX}/bin
 else
-    stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install pandoc
+    stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='$GHC_OPTIONS' pandoc
     mv ~/.local/bin/pandoc ${PREFIX}/bin
 fi
