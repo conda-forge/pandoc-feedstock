a01643aaf7e04d10ac8c51f06323e84fd31af517
Marco Rossi <developer@marco-rossi.com>
2022-09-26 14:08:07 +0200

    Move GHC_OPTIONS to build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
-stack install --ghc-options='%GHC_OPTIONS%' pandoc
+:: ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/.github/workflows/release-candidate.yml#L37
+stack install --ghc-options='-j4 +RTS -A256m -RTS' pandoc
 MKDIR %PREFIX%\bin
 MOVE %APPDATA%\local\bin\pandoc.exe %PREFIX%\bin\pandoc.exe
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,10 @@
 mkdir -p ${PREFIX}/bin
+if [[ "$target_platform" == linux-* ]]; then
+    # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/Makefile#L16
+    export GHC_OPTIONS="-fdiagnostics-color=always -j4 +RTS -A8m -RTS"
+elif [[ "$target_platform" == osx-* ]]; then
+    # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/.github/workflows/release-candidate.yml#L100
+    export GHC_OPTIONS="-j4 +RTS -A256m -RTS -split-sections"
 if [[ "$target_platform" == linux-aarch64 ]]; then
     mv bin/* ${PREFIX}/bin
 else
