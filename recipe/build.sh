1a62ac3ae3485c4f35d38e7e65336a1350595a6a
Marco Rossi <developer@marco-rossi.com>
2022-09-27 13:49:58 +0200

    Do not use environment variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,15 @@
 mkdir -p ${PREFIX}/bin
-if [[ "$target_platform" == linux-* ]]; then
-    # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/Makefile#L16
-    export GHC_OPTIONS="-fdiagnostics-color=always -j4 +RTS -A8m -RTS"
-elif [[ "$target_platform" == osx-* ]]; then
-    # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/.github/workflows/release-candidate.yml#L100
-    export GHC_OPTIONS="-j4 +RTS -A256m -RTS -split-sections"
-fi
 if [[ "$target_platform" == linux-aarch64 ]]; then
     mv bin/* ${PREFIX}/bin
 else
-    stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='$GHC_OPTIONS' pandoc
+    if [[ "$target_platform" == linux-* ]]; then
+        # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/Makefile#L16
+        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-fdiagnostics-color=always -j4 +RTS -A8m -RTS' pandoc
+    elif [[ "$target_platform" == osx-* ]]; then
+        # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/.github/workflows/release-candidate.yml#L100
+        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-j4 +RTS -A256m -RTS -split-sections' pandoc
+    else
+        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install pandoc
+    fi
     mv ~/.local/bin/pandoc ${PREFIX}/bin
 fi
