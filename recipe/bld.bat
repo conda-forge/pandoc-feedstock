06d16a91ac424b101dcc32cc977d2ba698f96fa1
Marco Rossi <developer@marco-rossi.com>
2023-03-08 17:50:40 +0100

    Update build target

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 :: ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/.github/workflows/release-candidate.yml#L37
-stack build -j4 +RTS -A256m -RTS --copy-bins pandoc
+stack build -j4 +RTS -A256m -RTS --copy-bins pandoc-cli
 MKDIR %LIBRARY_BIN%
 MOVE %APPDATA%\local\bin\pandoc.exe %LIBRARY_BIN%\pandoc.exe
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,10 @@ if [[ "$target_platform" == linux-aarch64 ]]; then
 else
     if [[ "$target_platform" == linux-* ]]; then
         # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/Makefile#L16
-        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-fdiagnostics-color=always -j4 +RTS -A8m -RTS' pandoc
+        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-fdiagnostics-color=always -j4 +RTS -A8m -RTS' pandoc-cli
     elif [[ "$target_platform" == osx-* ]]; then
         # ghc-options taken from https://github.com/jgm/pandoc/blob/2.19.2/.github/workflows/release-candidate.yml#L100
-        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-j4 +RTS -A256m -RTS -split-sections' pandoc
+        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-j4 +RTS -A256m -RTS -split-sections' pandoc-cli
     else
         stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install pandoc
     fi
