9fcdb9df4d25fe55577abbdf9efa2d8178b3301e
Marco Rossi <developer@marco-rossi.com>
2023-07-17 22:37:09 +0200

    Update build options

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,10 @@ if [[ "$target_platform" == linux-aarch64 ]] || [[ "$target_platform" == osx-arm
     mv bin/* ${PREFIX}/bin
 else
     if [[ "$target_platform" == linux-* ]]; then
-        # ghc-options taken from https://github.com/jgm/pandoc/blob/3.1.1/Makefile#L15
+        # ghc-options taken from https://github.com/jgm/pandoc/blob/3.1.4/Makefile#L15
         stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-fwrite-ide-info -fdiagnostics-color=always -j4 +RTS -A8m -RTS' pandoc-cli
-    elif [[ "$target_platform" == osx-* ]]; then
-        # ghc-options taken from https://github.com/jgm/pandoc/blob/3.1.1/.github/workflows/release-candidate.yml#L97
-        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --ghc-options='-j4 +RTS -A256m -RTS -split-sections' pandoc-cli
     else
-        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install pandoc-cli
+        stack --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib install --flag pandoc:embed_data_files --flag pandoc-cli:lua --flag pandoc-cli:server pandoc-cli
     fi
     mv ~/.local/bin/pandoc ${PREFIX}/bin
 fi
